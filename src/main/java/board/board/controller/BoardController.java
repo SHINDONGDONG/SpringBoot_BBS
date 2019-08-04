1737885ae9094285d19d0817af4f0a98b97640de
Shinmincheol <Shinmincheol@shinFamilly>
2019-08-04 13:09:19 +0900

    0804업데이트

--- src/main/java/board/board/controller/BoardController.java
+++ src/main/java/board/board/controller/BoardController.java
@@ -2,6 +2,8 @@ package board.board.controller;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,16 @@ import board.board.service.BoardService;
 
 @Controller
 public class BoardController {
-
+	
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private BoardService boardService;
 	
 	@RequestMapping("/board/openBoardList.do")
 	public ModelAndView openBoardList() throws Exception{
+		log.debug("openBoardList");
+		
 		ModelAndView mv = new ModelAndView("/board/boardList");
 		
 		List<BoardDto> list = boardService.selectBoardList();
@@ -40,6 +46,8 @@ public class BoardController {
 	
 	@RequestMapping("/board/openBoardDetail.do")
 		public ModelAndView openBoardDetail(@RequestParam int boardIdx)throws Exception {
+			log.debug("openBoardDetail");
+		
 			ModelAndView mv = new ModelAndView("/board/boardDetail");
 			boardService.updateHitCount(boardIdx);
 			BoardDto board = boardService.selectBoardDetail(boardIdx);
